1f6fcd9cd216ce9daa2e0f9a4fcd14c29383f614
LaCodon <webmaster@lacodon.de>
2016-01-21 10:18:47 +0100

    Started installer

---
+++ install.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright 2016 | Licensed under MIT
+ * 
+ * Currently additionally required extensions for MyEnvoy:
+ *  - apc
+ *  - acpu
+ *  - curl
+ *  - gd
+ *  - mcrypt
+ *  - mhash
+ * 
+ */
+echo 'Welcome to the MyEnvoy installer script!' . PHP_EOL;
+echo 'This PHP script will lead you through the installation process of an envoy on your machine.' . PHP_EOL;
+echo '' . PHP_EOL;
