ab589e18382c03791f2b129f086ddc00ba026666
LaCodon <webmaster@lacodon.de>
2016-01-22 13:45:43 +0100

    Get all required extensions

--- install.php
+++ install.php
@@ -12,6 +12,21 @@
  *  - mhash
  * 
  */
+$_required_extensions = array('apc', 'apcu', 'curl', 'gd', 'mcrypt', 'mhash');
+$_havetoinstall = array();
+
+
 echo 'Welcome to the MyEnvoy installer script!' . PHP_EOL;
-echo 'This PHP script will lead you through the installation process of an envoy on your machine.' . PHP_EOL;
+echo 'This PHP script will lead you through the installation process of an envoy on your machine.' . PHP_EOL . PHP_EOL;
+echo 'At first, all missing PHP extensions will get installed.' . PHP_EOL;
+echo 'Search for installed extensions . . .' . PHP_EOL;
+
+foreach ($_required_extensions as $ext) {
+    if (extension_loaded($ext) === FALSE) {
+        $_havetoinstall[] = $ext;
+    }
+}
+
+echo 'The following missing extensions will get installed:' . PHP_EOL;
+echo implode(', ', $_havetoinstall) . PHP_EOL;
 echo '' . PHP_EOL;
